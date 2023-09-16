cdbeb015d3db4becac3a14b9969e2c42b11a2ff4
Vladimir Pirozhenko <vvpirozhok@mail.ru>
2023-09-16 16:34:36 +0300

    Added hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        //Variable: //DataType Name (Переменная: Тип данных Имя)
+        String welcomeStr; //DECLARATION(Обьявление переменной)
+        welcomeStr = "Hello!";//INITIALIZATION (Инициализация переменной)
+        System.out.println(welcomeStr);
+        String goodbyeStr = "Goodbye!";
+        String result = welcomeStr + goodbyeStr;
+        System.out.println(result);
+    }
+
+}
